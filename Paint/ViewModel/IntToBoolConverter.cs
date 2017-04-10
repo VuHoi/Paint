22636e87451ac19967384974dcaafa57e44606c9
hoideptrai123 <toilahoi007@gmail.com>
2017-04-11 00:36:30 +0700

    save Image

---
+++ Paint/ViewModel/IntToBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Paint.ViewModel
+{
+    [ValueConversion(typeof(string), typeof(int))]
+    public class IntToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type t, object parameter, CultureInfo culture)
+        {
+            return value.Equals(parameter);
+        }
+        public object ConvertBack(object value, Type t, object parameter, CultureInfo culture)
+        {
+            return value.Equals(false) ? DependencyProperty.UnsetValue : parameter;
+        }
+    }
+}
